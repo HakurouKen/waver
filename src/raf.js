71d9070d1626ae03dc62c8f72c320d471a18655e
hakurouken <ticshot@gmail.com>
2017-01-20 01:50:15 +0800

    requestAnimationFrame lib.

---
+++ src/raf.js
@@ -0,0 +1,49 @@
+(function(global){
+    'use strict';
+    // Reference: https://gist.github.com/paulirish/1579671
+
+    global = global || {};
+    const prefix = ['ms','moz','webkit','o'];
+    let requestAnimationFrame = global.requestAnimationFrame;
+    let cancelAnimationFrame = global.cancelAnimationFrame;
+    for (let i = 0; i < prefix.length && requestAnimationFrame; i++) {
+        requestAnimationFrame = global[prefix[i]+'RequestAnimationFrame'];
+        cancelAnimationFrame = global[prefix[i]+'CancelAnimationFrame']
+                                || global[prefix[i]+'CancelRequestAnimationFrame'];
+    }
+
+    let lastTime = 0;
+    requestAnimationFrame = requestAnimationFrame || function(callback) {
+        let curTime = new Date().getTime();
+        let wait = Math.max(0, 16 - (curTime - lastTime));
+        let id = setTimeout(() => {
+            callback(curTime + wait);
+        }, wait);
+        lastTime = curTime + wait;
+        return id;
+    };
+
+    cancelAnimationFrame = cancelAnimationFrame || function(id) {
+        return clearTimeout(id);
+    };
+
+    module.exports = {
+        start: requestAnimationFrame,
+        stop: cancelAnimationFrame,
+        cycle: function(func) {
+            return new Promise((resolve, reject) => {
+                let funcWrapper = function() {
+                    requestAnimationFrame(time => {
+                        let next = func();
+                        if (next) {
+                            funcWrapper();
+                        } else {
+                            resolve();
+                        }
+                    });
+                };
+                funcWrapper();
+            });
+        }
+    };
+})(typeof window === 'undefined' ? window : this);
