1d4dcae69158509aaa739b7892466835a0da1af7
hakurouken <ticshot@gmail.com>
2017-01-22 01:06:16 +0800

    Demos.

---
+++ src/index.js
@@ -0,0 +1,35 @@
+'use strict';
+
+import raf from './raf';
+import AudioAnalyser from './analyser';
+import StripeDrawer from './drawer/stripe';
+import WaveFormDrawer from './drawer/waveform';
+
+const $ = document.getElementById.bind(document);
+let audio = $('audio');
+let stripeCanvas = $('stripe-canvas');
+let waveformCanvas = $('waveform-canvas');
+let uploader = $('upload');
+
+let analyser = new AudioAnalyser(audio);
+let stripeDrawer = new StripeDrawer(stripeCanvas);
+let waveformDrawer = new WaveFormDrawer(waveformCanvas);
+
+uploader.onchange = function() {
+  let files = this.files;
+  if (files[0]) {
+    if (audio.src) {
+      URL.revokeObjectURL(audio.src);
+    }
+    audio.src = URL.createObjectURL(files[0]);
+    audio.play();
+  }
+};
+
+raf.cycle(() => {
+  let stripeData = analyser.getFrequencyData(64);
+  let waveformData = analyser.getTimeDomainData(64);
+  stripeDrawer.draw({ data: stripeData });
+  waveformDrawer.draw({ data: waveformData });
+  return true;
+});
