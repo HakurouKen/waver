82cfdd56f5beea16b1403f1e522db7e3d1acbaeb
HakurouKen <ticshot@gmail.com>
2017-01-22 15:15:35 +0800

    CircleDrawer demo.

--- src/index.js
+++ src/index.js
@@ -4,16 +4,19 @@ import raf from './raf';
 import AudioAnalyser from './analyser';
 import StripeDrawer from './drawer/stripe';
 import WaveFormDrawer from './drawer/waveform';
+import CircleDrawer from './drawer/circle';
 
 const $ = document.getElementById.bind(document);
 let audio = $('audio');
 let stripeCanvas = $('stripe-canvas');
 let waveformCanvas = $('waveform-canvas');
+let circleCanvas = $('circle-canvas');
 let uploader = $('upload');
 
 let analyser = new AudioAnalyser(audio);
 let stripeDrawer = new StripeDrawer(stripeCanvas);
 let waveformDrawer = new WaveFormDrawer(waveformCanvas);
+let circleDrawer = new CircleDrawer(circleCanvas);
 
 uploader.onchange = function() {
   let files = this.files;
@@ -31,5 +34,6 @@ raf.cycle(() => {
   let waveformData = analyser.getTimeDomainData(64);
   stripeDrawer.draw({ data: stripeData });
   waveformDrawer.draw({ data: waveformData });
+  circleDrawer.draw({ data: waveformData });
   return true;
 });
